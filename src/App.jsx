9cfa62fc76461dfd442ddbd7419a55c2bf594cb8
Brayan LP <brayansystemlp@gmail.com>
2022-07-31 22:27:37 -0500

    :tada :actualizando material tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React, { useState, Fragment, useEffect } from "react";
 import { Popover, Transition } from "@headlessui/react";
 import { MenuIcon } from "@heroicons/react/outline";
+import { LockClosedIcon } from "@heroicons/react/solid";
+
 import { db } from "./utils/firebase.js";
 import {
   collection,
@@ -11,6 +13,15 @@ import {
   serverTimestamp,
   getDocs,
 } from "firebase/firestore";
+
+import {
+  Tabs,
+  TabsHeader,
+  TabsBody,
+  Tab,
+  TabPanel,
+} from "@material-tailwind/react";
+
 import "./App.css";
 
 const request = collection(db, "usuarios");
@@ -19,9 +30,9 @@ const request1 = query(collection(db, "usuarios"));
 function App() {
   const [todos, setTodos] = useState([]);
 
-  useEffect( ()=>{
+  useEffect(() => {
+    console.log(":::CARGANDO USEEFFECT::");
     getData();
-
   }, []);
 
   // const getDataSnapshot = () =>{
@@ -30,13 +41,51 @@ function App() {
   //   })
   // }
 
-  const getData = async()=>{
+  const getData = async () => {
     const response = await getDocs(request);
     const list = response.docs.map((doc) => doc.data());
     setTodos(list);
-  }
- 
-  console.log('todos', todos);
+  };
+
+  const data = [
+    {
+      label: "HTML",
+      value: "html",
+      desc: `It really matters and then like it really doesn't matter.
+      What matters is the people who are sparked by it. And the people 
+      who are like offended by it, it doesn't matter.`,
+    },
+    {
+      label: "React",
+      value: "react",
+      desc: `Because it's about motivating the doers. Because I'm here
+      to follow my dreams and inspire other people to follow their dreams, too.`,
+    },
+
+    {
+      label: "Vue",
+      value: "vue",
+      desc: `We're not always in the position that we want to be at.
+      We're constantly growing. We're constantly making mistakes. We're 
+      constantly trying to express ourselves and actualize our dreams.`,
+    },
+
+    {
+      label: "Angular",
+      value: "angular",
+      desc: `Because it's about motivating the doers. Because I'm here
+      to follow my dreams and inspire other people to follow their dreams, too.`,
+    },
+
+    {
+      label: "Svelte",
+      value: "svelte",
+      desc: `We're not always in the position that we want to be at.
+      We're constantly growing. We're constantly making mistakes. We're 
+      constantly trying to express ourselves and actualize our dreams.`,
+    },
+  ];
+
   return (
     <>
       <Popover className="relative bg-white">
@@ -94,13 +143,151 @@ function App() {
         Button
       </button>
 
-
       <br />
       <br />
 
       <ul>
-          {todos.map((val, index)=> <li   key={index + 1}>{val.nombre}</li>)}
+        {todos.map((val, index) => (
+          <div key={index + 1}>
+            <div className="max-w-sm rounded overflow-hidden shadow-lg card-menu">
+              <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">{val.nombre}</div>
+              </div>
+              <img
+                className="w-full"
+                src="https://via.placeholder.com/500"
+                alt="Sunset in the mountains"
+              />
+              <div className="px-6 py-4">
+                <div className="font-bold text-l mb-2">S/20.00</div>
+              </div>
+              <div className="hover-card">
+                <img src="" alt="Candela" />
+              </div>
+            </div>
+          </div>
+        ))}
       </ul>
+
+      <br />
+      <br />
+      <Tabs value="html">
+        <TabsHeader>
+          {data.map(({ label, value }) => (
+            <Tab key={value} value={value}>
+              {label}
+            </Tab>
+          ))}
+        </TabsHeader>
+        <TabsBody>
+          {data.map(({ value, desc }) => (
+            <TabPanel key={value} value={value}>
+              {desc}
+            </TabPanel>
+          ))}
+        </TabsBody>
+      </Tabs>
+
+      <br />
+      <br />
+
+      <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-md w-full space-y-8">
+          <div>
+            <img
+              className="mx-auto h-12 w-auto"
+              src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+              alt="Workflow"
+            />
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+              Sign in to your account
+            </h2>
+            <p className="mt-2 text-center text-sm text-gray-600">
+              Or{" "}
+              <a
+                href="#"
+                className="font-medium text-indigo-600 hover:text-indigo-500"
+              >
+                start your 14-day free trial
+              </a>
+            </p>
+          </div>
+          <form className="mt-8 space-y-6" action="#" method="POST">
+            <input type="hidden" name="remember" defaultValue="true" />
+            <div className="rounded-md shadow-sm -space-y-px">
+              <div>
+                <label htmlFor="email-address" className="sr-only">
+                  Email address
+                </label>
+                <input
+                  id="email-address"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Email address"
+                />
+              </div>
+              <div>
+                <label htmlFor="password" className="sr-only">
+                  Password
+                </label>
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Password"
+                />
+              </div>
+            </div>
+
+            <div className="flex items-center justify-between">
+              <div className="flex items-center">
+                <input
+                  id="remember-me"
+                  name="remember-me"
+                  type="checkbox"
+                  className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+                />
+                <label
+                  htmlFor="remember-me"
+                  className="ml-2 block text-sm text-gray-900"
+                >
+                  Remember me
+                </label>
+              </div>
+
+              <div className="text-sm">
+                <a
+                  href="#"
+                  className="font-medium text-indigo-600 hover:text-indigo-500"
+                >
+                  Forgot your password?
+                </a>
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                  <LockClosedIcon
+                    className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                    aria-hidden="true"
+                  />
+                </span>
+                Sign in
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </>
   );
 }
